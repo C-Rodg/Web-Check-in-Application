885fdac62a774a89af2edd12eeb37be0c12610a9
C-Rodg <rodgersbc@gmail.com>
2016-11-17 12:37:23 -0800

    Setup Webpack configuration

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { hashHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
